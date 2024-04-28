88d3ba52b64fbfbb12f9da2b7c4c8e93ee888d6a
Admin <ibraheem.badran9@gmail.com>
2024-04-28 14:54:36 +0300

    test

--- app/src/main/java/badran/ibraheem/ibraheemtaskslists/MainActivity.java
+++ app/src/main/java/badran/ibraheem/ibraheemtaskslists/MainActivity.java
@@ -1,8 +1,13 @@
 package badran.ibraheem.ibraheemtaskslists;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +17,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.splashscreen);
         //koko wawa
     }
+
+    @Override//بناء القائمه
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override //معالجة حدث اختيار عنصر من القائمة
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId()==R.id.itmSettings)
+        {
+        }
+        if(item.getItemId()==R.id.itmSignOut)
+        {
+        }
+        return true;
+    }
+
+    /**
+     * داله مساعده لفتح قائمه تتلقى بارمتر للكائن الذي سبب فتح القائمه
+     * @param v
+     */
+    public void showPopUpMenu(View v)
+    {
+        PopupMenu popup=new PopupMenu(this,v);
+        popup.inflate(R.menu.popup_menu);
+        popup.show();
+    }
+
+    public void onClick(View v){
+        showPopUpMenu(v);}
+
+
+
+
+
+
+
+
+
 }
